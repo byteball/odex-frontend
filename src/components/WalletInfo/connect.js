b24e41a7a549c9f0f0db2aa1bc174c2c94981b3d
Jikun Liao <liaojikundever@gmail.com>
2020-06-03 06:28:41 -0400

    remove unnecessary diffs

--- src/components/WalletInfo/connect.js
+++ src/components/WalletInfo/connect.js
@@ -1,20 +1,22 @@
-import { connect } from 'react-redux';
-import walletInfoSelector, { detectToken, addToken, registerToken } from '../../store/models/walletInfo';
-import getTransferTokensFormSelector from '../../store/models/transferTokensForm';
+import { connect } from 'react-redux'
+import walletInfoSelector, { detectToken, addToken, registerToken } from '../../store/models/walletInfo'
+import getTransferTokensFormSelector from '../../store/models/transferTokensForm'
+
 
 const mapStateToProps = (state: State) => {
-  const transferTokensFormSelector = getTransferTokensFormSelector(state);
-  let selector = walletInfoSelector(state);
+  let selector = walletInfoSelector(state)
+  const transferTokensFormSelector = getTransferTokensFormSelector(state)
+
   return {
-    ...selector,
-    exchangeAddress: transferTokensFormSelector.exchangeAddress(),
-  };
-};
+      ...selector,
+      exchangeAddress: transferTokensFormSelector.exchangeAddress()
+  }
+}
 
 const mapDispatchToProps = {
   detectToken,
   addToken,
   registerToken,
-};
+}
 
-export default connect(mapStateToProps, mapDispatchToProps);
+export default connect(mapStateToProps, mapDispatchToProps)
