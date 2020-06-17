fd5dcdb1522ed1837979fcd7ded974947e82beaa
Jikun Liao <liaojikundever@gmail.com>
2020-06-17 05:34:54 -0400

    remove unnecessary diffs

--- src/components/WalletInfo/WalletInfoRenderer.js
+++ src/components/WalletInfo/WalletInfoRenderer.js
@@ -2,18 +2,18 @@
 import React from 'react';
 import styled from 'styled-components';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import TokenBalanceChart from '../../components/TokenBalanceChart';
-import RecentTxTable from '../../components/RecentTxTable';
-import Help from '../../components/Help';
+import TokenBalanceChart from '../../components/TokenBalanceChart'
+import RecentTxTable from '../../components/RecentTxTable'
+import Help from '../../components/Help'
 
 import { Card, Position, Button, Tag, Tabs, Tab, InputGroup, Icon } from '@blueprintjs/core';
-import { Flex, FlexRow, FlexItem, Box, Colors, Text, TextDiv, TextBox, BlueGlowingButton, ModalBody } from '../Common';
-import { Fonts } from '../Common/Variables';
-import { Spring } from 'react-spring';
+import { Flex, FlexRow, FlexItem, Box, Colors, Text, TextDiv, TextBox, BlueGlowingButton, ModalBody } from '../Common'
+import { Fonts } from '../Common/Variables'
+import { Spring } from 'react-spring'
 import Modal from '../Modal';
 
 import type { TokenData } from '../../types/tokens';
-import type { Tx } from '../../types/transactions';
+import type { Tx } from '../../types/transactions'
 
 import { PROTOCOL } from '../../config/urls';
 import TransactionsTable from './Transactions';
@@ -32,16 +32,16 @@ type Props = {
   accountExplorerUrl: string,
   address: string,
   authorizations: Array,
-  handleChangeAddress: (SyntheticInputEvent<Object>) => void,
+  handleChangeAddress: SyntheticInputEvent<Object> => void,
   tokenIsAdded: ?boolean,
   tokenIsListed: ?boolean,
   tokenIsRegistered: ?boolean,
   handleChangeTab: string => void,
   handleModalClose: void => void,
-  handleChangeAsset: (SyntheticInputEvent<Object>) => void,
-  handleDetectToken: (SyntheticEvent<>) => Promise<void>,
-  handleAddToken: (SyntheticEvent<>) => Promise<void>,
-  handleRegisterToken: (SyntheticEvent<>) => Promise<void>,
+  handleChangeAsset: SyntheticInputEvent<Object> => void,
+  handleDetectToken: SyntheticEvent<> => Promise<void>,
+  handleAddToken: SyntheticEvent<> => Promise<void>,
+  handleRegisterToken: SyntheticEvent<> => Promise<void>,
   addTokenPending: boolean,
   registerTokenPending: boolean,
   recentTransactions: Array<Tx>,
@@ -87,21 +87,21 @@ const WalletInfoRenderer = (props: Props) => {
   } = props;
 
   return (
-    <WalletInfoCard>
+      <WalletInfoCard>
       <ButtonRow>
         <Button
           text="Portfolio"
           minimal
-          onClick={() => handleChangeTab('Portfolio')}
-          active={selectedTab === 'Portfolio'}
-          intent={selectedTab === 'Portfolio' ? 'primary' : ''}
+          onClick={() => handleChangeTab("Portfolio")}
+          active={selectedTab === "Portfolio"}
+          intent={selectedTab === "Portfolio" ? 'primary' : ''}
         />
         <Button
           text="Add Token"
           minimal
-          onClick={() => handleChangeTab('Add Token')}
-          active={selectedTab === 'Add Token'}
-          intent={selectedTab === 'Add Token' ? 'primary' : ''}
+          onClick={() => handleChangeTab("Add Token")}
+          active={selectedTab === "Add Token"}
+          intent={selectedTab === "Add Token" ? 'primary' : ''}
         />
         <Button
           text="Authorizations"
@@ -191,197 +191,211 @@ const WalletInfoRenderer = (props: Props) => {
             />
           }
         />} */}
-      </Tabs>
-    </WalletInfoCard>
+      </Tabs>        
+      </WalletInfoCard>
   );
 };
 
 const PortfolioPanel = (props: *) => {
-  const { accountAddress, accountExplorerUrl, transactions, tokenData, exchangeAddress } = props;
+  const {
+    accountAddress,
+    accountExplorerUrl,
+    transactions,
+    tokenData,
+    exchangeAddress
+  } = props
 
   return (
     <Box>
-      <Box py={0}>
-        <TextBox>
-          <h3 minimal large>
-            Obyte Account
-          </h3>
-        </TextBox>
-        <TextDiv py={2} small muted>
-          Here is your Obyte address that is linked to the exchange. Your deposits should come from this address and
-          your withdrawals will be also sent to this address.
-        </TextDiv>
-        <FlexRow py={2} px={0} fontSize={Fonts.FONT_SIZE_SMALL}>
-          <a href={accountExplorerUrl} target="_blank" rel="noopener noreferrer" className="bp3-text-overflow-ellipsis">
-            {accountAddress}
-          </a>
-          <CopyToClipboardBox px={1}>
-            <CopyToClipboard text={accountAddress}>
-              <Icon icon="clipboard" intent="primary" />
-            </CopyToClipboard>
-          </CopyToClipboardBox>
-        </FlexRow>
-      </Box>
-      <h3 minimal large>
-        Portfolio Overview
-      </h3>
-      <TokenBalanceChartBox>
-        <TokenBalanceChart />
-      </TokenBalanceChartBox>
-      <h3 minimal large>
-        Recent Transactions
-      </h3>
-      <Box my={2}>
-        <TransactionsTable
-          transactions={transactions}
-          accountAddress={accountAddress}
-          tokenData={tokenData}
-          exchangeAddress={exchangeAddress}
-        />
-      </Box>
-    </Box>
-  );
-};
+        <Box py={0}>
+          <TextBox>
+            <h3 minimal large>Obyte Account</h3>
+          </TextBox>
+          <TextDiv py={2} small muted>
+            Here is your Obyte address that is linked to the exchange.  Your deposits should come from this address and your withdrawals will be also sent to this address.
+          </TextDiv>
+          <FlexRow 
+            py={2} 
+            px={0} 
+            fontSize={Fonts.FONT_SIZE_SMALL} 
+          >
+            <a href={accountExplorerUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bp3-text-overflow-ellipsis"
+            >
+              {accountAddress}
+            </a>
+            <CopyToClipboardBox px={1}>
+              <CopyToClipboard text={accountAddress}>
+                <Icon icon="clipboard" intent="primary" />
+              </CopyToClipboard>
+            </CopyToClipboardBox>
+          </FlexRow>
+        </Box>
+        <h3 minimal large>Portfolio Overview</h3>
+        <TokenBalanceChartBox>
+          <TokenBalanceChart />
+        </TokenBalanceChartBox>
+        <h3 minimal large>Recent Transactions</h3>
+          <Box my={2}>
+            <TransactionsTable 
+              transactions={transactions}
+              accountAddress={accountAddress}
+              tokenData={tokenData}
+              exchangeAddress={exchangeAddress}
+            />
+          </Box>
+        </Box>
+  )
+}
 
 const CopyToClipboardBox = styled(Box)`
   cursor: context-menu;
-`;
+`
 
 const AddTokenPanel = (props: *) => {
-  const {
+  const { 
     asset,
-    assetStatus,
+    assetStatus, 
     tokenSymbol,
     tokenExplorerUrl,
     tokenIsAdded,
     tokenIsListed,
     tokenIsRegistered,
-    handleDetectToken,
-    handleChangeAsset,
+    handleDetectToken,    
+    handleChangeAsset, 
     handleAddToken,
     handleRegisterToken,
     addTokenPending,
-    registerTokenPending,
-  } = props;
+    registerTokenPending
+  } = props
 
   return (
-    <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
-      {props => (
+      <Spring from={{opacity: 0}} to={{opacity: 1}}>
+      {props =>
         <Box style={props}>
-          <Text muted>
-            Add a token that is not listed among the default ODEX tokens. If the token has not yet been added to the
-            ODEX you can also list the token.
-            <br />
-            View the FAQ for more detailed information on listing tokens.
-          </Text>
-          <Flex py={3}>
+        <Text muted>
+          Add a token that is not listed among the default ODEX tokens. If the token has not yet been added to 
+          the ODEX you can also list the token.
+          <br />
+          View the FAQ for more detailed information on listing tokens.
+        </Text>
+        <Flex py={3}>
             <FlexItem flex="1">
               <InputGroup
                 name="asset"
                 placeholder="asset ID (44 characters) or symbol"
-                intent={assetStatus === 'invalid' ? 'danger' : ''}
+                intent={assetStatus === "invalid" ? "danger" : ""}
                 onChange={handleChangeAsset}
                 value={asset}
                 autoFocus
                 fill
               />
             </FlexItem>
-            <Button intent="primary" text="Search Token" minimal onClick={handleDetectToken} />
-            <Box pl={2} style={{ display: 'flex', alignItems: 'center' }}>
-              <Help position={Position.LEFT}>This button will detect whether the entered asset exists.</Help>
-            </Box>
-          </Flex>
-          {tokenSymbol && (
-            <Box>
-              <h3>Token found: {tokenSymbol}</h3>
-              <a href={tokenExplorerUrl} target="_blank">
-                → View on Explorer
-              </a>
-              <Box py={3}>
-                {tokenIsRegistered && (
-                  <Flex py={1} width="50%">
-                    <BlueGlowingButton
-                      disabled={tokenIsAdded}
-                      text={tokenIsAdded ? 'Token added' : 'Add token'}
-                      loading={addTokenPending}
-                      intent="primary"
-                      fill
-                      onClick={handleAddToken}
-                    />
-                    <Box pl={2} pt={1}>
-                      <Help position={Position.RIGHT}>
-                        Add {tokenSymbol} to your list of tradable tokens.
-                        <br />
-                        <br />
-                        If another person wants to trade {tokenSymbol} too, that person needs to add this token on their
-                        own account to discover the corresponding markets.
-                        <br />
-                        <br />
-                      </Help>
-                    </Box>
-                  </Flex>
-                )}
-                {!tokenIsRegistered && (
-                  <Flex py={1} width="50%">
-                    <BlueGlowingButton
-                      disabled={tokenIsRegistered}
-                      text={tokenIsRegistered ? 'Token registered' : 'Register token'}
-                      loading={registerTokenPending}
-                      intent="primary"
-                      fill
-                      onClick={handleRegisterToken}
-                    />
-                    <Box pl={2} pt={1}>
-                      <Help position={Position.RIGHT}>
-                        Registering a token will create the following markets:
-                        <ul>
-                          <li>→ {tokenSymbol}/USDC</li>
-                          <li>→ {tokenSymbol}/GBYTE</li>
-                        </ul>
-                        If another person wants to trade {tokenSymbol}, that person needs to add this token on their own
-                        account to discover the corresponding markets.
-                        <br />
-                        <br />
-                      </Help>
-                    </Box>
-                  </Flex>
-                )}
+          <Button
+            intent="primary"
+            text="Search Token"
+            minimal
+            onClick={handleDetectToken}
+          />
+          <Box pl={2} style={{display: "flex", alignItems: "center"}}>
+            <Help position={Position.LEFT}>
+              This button will detect whether the entered asset exists.
+            </Help>
+          </Box>
+      </Flex>
+      {tokenSymbol &&
+        <Box>
+          <h3>Token found: {tokenSymbol}</h3>
+          <a href={tokenExplorerUrl} target="_blank">→ View on Explorer</a>
+          <Box py={3}>
+            {tokenIsRegistered && <Flex py={1} width="50%">
+              <BlueGlowingButton
+                disabled={tokenIsAdded}
+                text={tokenIsAdded ? "Token added" : "Add token"}
+                loading={addTokenPending}
+                intent="primary"
+                fill
+                onClick={handleAddToken}
+              />
+              <Box pl={2} pt={1}>
+                <Help position={Position.RIGHT}>
+                  Add {tokenSymbol} to your list of tradable tokens.
+                  <br />
+                  <br />
+
+                  If another person wants to trade {tokenSymbol} too, that person needs to add this token on their own account to discover
+                  the corresponding markets.
+                  <br />
+                  <br />
+                </Help>
               </Box>
-            </Box>
-          )}
+            </Flex>}
+            {!tokenIsRegistered && <Flex py={1} width="50%">
+              <BlueGlowingButton
+                disabled={tokenIsRegistered}
+                text={tokenIsRegistered ? "Token registered" : "Register token"}
+                loading={registerTokenPending}
+                intent="primary"
+                fill
+                onClick={handleRegisterToken}
+              />
+              <Box pl={2} pt={1}>
+                <Help position={Position.RIGHT}>
+                  Registering a token will create the following markets:
+                  <ul>
+                    <li>→ {tokenSymbol}/USDC</li>
+                    <li>→ {tokenSymbol}/GBYTE</li>
+                  </ul>
+                  If another person wants to trade {tokenSymbol}, that person needs to add this token on their own account to discover
+                  the corresponding markets.
+                  <br />
+                  <br />
+                </Help>
+              </Box>
+            </Flex>}
+          </Box>
         </Box>
-      )}
+        }
+      </Box>
+      }
     </Spring>
-  );
-};
+  )
+}
+
 
 const PremiumListingPanel = (props: *) => {
-  return <Box>For premium listing, contact us at support@proofsuite.com</Box>;
-};
+  return (
+    <Box>
+      For premium listing, contact us at support@proofsuite.com
+    </Box>
+  )
+}
 const WalletInfoCard = styled(Card)`
   height: 92vh;
   overflow-y: auto;
-`;
+`
 
 const GlowingButton = styled(Button)`
-  box-shadow: ${'0 3px 20px ' + Colors.BLUE1 + '!important;'}
+  box-shadow: ${"0 3px 20px " + Colors.BLUE1 + "!important;"}
   &hover: {
     background-color: ${Colors.BLUE5}
-    box-shadow: ${'0 3px 20px ' + Colors.BLUE5 + '!important;'}
+    box-shadow: ${"0 3px 20px " + Colors.BLUE5 + "!important;"}
   }
-`;
+`
 
 const TokenBalanceChartBox = styled.div`
   display: flex;
+  margin-bottom: 10px;
   justify-content: left;
   align-content: middle;
   text-align: center;
   width: 100%;
-  margin-bottom: 10px;
   svg {
     overflow: visible;
   }
-`;
+`
 
 const ButtonRow = styled.span`
   display: flex;
@@ -389,21 +403,14 @@ const ButtonRow = styled.span`
   & .bp3-button {
     margin-left: 5px;
   }
-`;
+`
 
 const GRANTTEXT = styled.div`
   word-break: break-word;
 `;
 
 const AuthorizationsPanel = (props: *) => {
-  const {
-    address,
-    authorizations,
-    handleChangeAddress,
-    exchangeAddress,
-    handleToggleRevokeModal,
-    accountAddress,
-  } = props;
+  const { address, authorizations, handleChangeAddress, exchangeAddress, handleToggleRevokeModal, accountAddress } = props;
   let data = {
     grant: 1,
     address,
@@ -411,12 +418,9 @@ const AuthorizationsPanel = (props: *) => {
   let base64data = btoa(JSON.stringify(data));
   const link = PROTOCOL + exchangeAddress + '?amount=10000&base64data=' + encodeURIComponent(base64data);
 
-  const isValid = isValidAddress(address) && ![accountAddress, ...authorizations].includes(address);
-  const message = !isValidAddress(address)
-    ? 'Please input a valid address'
-    : address === accountAddress
-    ? `You can't authorize your wallet address`
-    : `You can't authorize the address that you authorized in the past`;
+  const isValid = isValidAddress(address) && ![accountAddress, ...authorizations].includes(address)
+  const message = !isValidAddress(address) ? 'Please input a valid address' 
+    : address === accountAddress ? `You can't authorize your wallet address`: `You can't authorize the address that you authorized in the past`
 
   return (
     <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
@@ -429,6 +433,7 @@ const AuthorizationsPanel = (props: *) => {
           {authorizations.length === 0 && <Text muted>You don't have any authorized addresses</Text>}
 
           {authorizations.map(element => {
+
             return (
               <FlexRow py={2} key={element} alignItems="center">
                 <FlexItem flex="1">
@@ -462,11 +467,7 @@ const AuthorizationsPanel = (props: *) => {
               }}
             />
           </Flex>
-          {!isValid && address !== '' && (
-            <Text muted intent="danger">
-              {message}
-            </Text>
-          )}
+          {!isValid && address !== '' && <Text muted intent="danger">{message}</Text>}
         </Box>
       )}
     </Spring>
